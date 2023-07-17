cff9ed5a79e2bf029dc415c46df5c15ab3718142
Raj Shinde <shinderaj85@gmail.com>
2023-07-17 09:19:04 +0200

    Integration with Jenkins

--- src/test/java/api/testrunner/TestRunner.java
+++ src/test/java/api/testrunner/TestRunner.java
@@ -7,11 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/api/features",
-		glue={"src/test/java/api/stepdefinition"},
+		glue={"api.stepdefinition"},
 		dryRun = false,
 		monochrome = true,
 		tags = "@Smoke",
-		plugin = {"pretty","html:target/cucumber-reports","json:target/jsonReports/cucumber-report.json"
+		plugin = {"pretty","html:target/cucumber-reports/report.html","json:target/jsonReports/cucumber-report.json"
 				},
 		publish = true
 )
